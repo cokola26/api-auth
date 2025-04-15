91ec1a2624534656350c902b7dc31b8edc4e695a
cokola26 <boudet.coralie26@gmail.com>
2025-04-15 15:26:56 +0200

    update user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,9 +4,7 @@ export const getUserProfile = async (req, res) => {
     const {id} = req.user
     try {
         const userByID = await User.findById(id).select('-password')
-        if(!userByID){
-            return res.status(404).json(`User not found`)
-        }
+        
         return res.status(200).json(userByID)
 
     } catch (err) {
